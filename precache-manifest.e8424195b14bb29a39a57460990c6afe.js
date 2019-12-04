7e3fffbbd478634b6deec00368050a439d2905c5
Hrant-p <hrant@live.ru>
2019-12-05 00:21:51 +0400

    Updates

---
+++ precache-manifest.e8424195b14bb29a39a57460990c6afe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544b6e6ae5fc8be8e71c4d8fe3132ca1",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "c84322268c6a8994f71d",
+    "url": "/todo-list/static/css/main.6aaad8a2.chunk.css"
+  },
+  {
+    "revision": "fd8afd196e488b03ea3c",
+    "url": "/todo-list/static/js/2.4bfa2782.chunk.js"
+  },
+  {
+    "revision": "c84322268c6a8994f71d",
+    "url": "/todo-list/static/js/main.5c60f1af.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
